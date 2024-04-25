1c41ed8367a24c7560e2bf9e3b921791690d0a0a
Haizy <168135396+HaizyWorms@users.noreply.github.com>
2024-04-26 00:32:27 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,79 +1,82 @@
 import discord
-import random
-from discord import Game
-import asyncio
-from discord.ext.commands import Bot
-from discord.ext.commands import has_permissions 
-from discord import utils
 from discord.ext import commands
-
 import config
 
-client = commands.Bot(command_prefix="t!", intents=discord.Intents.all())  # Префикс бота
-client.remove_command("help") # Удаление стандарт Хелп
+bot = commands.Bot(command_prefix="t!", intents=discord.Intents.all())  # Префикс бота
+bot.remove_command("help")  # Удаление ds хелп
 
+# logs
+async def log_action(action, moderator, target):
+    guild = moderator.guild
+    log_channel = guild.get_channel(config.LOG_CHANNEL_ID)
+    if log_channel is not None:
+        await log_channel.send(f'Администратор {moderator} выполнил {action} над {target}')
+    else:
+        print("Ошибка: Канал логирования не найден!")
 
-# Надпись при включении бота
-@client.event
+# запретки
+forbidden_words = ["test2", "test1", "test"]  
+
+# help
+@bot.command()
+async def help(ctx):
+    await ctx.send('Вызвать данное меню - t!help.')
+    await ctx.send('Список доступных команд для модерации: t!mute , t!kick , t!ban , t!clear')
+    await ctx.send('Список доступных команд для развлечения: t!say')
+
+# tech_on
+@bot.event
 async def on_ready():
     print("Бот готов к работе!")
-
-    # Активность бота
-    await client.change_presence(
+    # Активность
+    await bot.change_presence(
         status=discord.Status.idle, activity=discord.Game("Помощь: t!help")
     )
 
-
-# Бан пользователя
-@client.command(pass_context=True, name="ban")
-@commands.has_permissions( administrator = True )
+# ban
+@bot.command()
+@commands.has_permissions(administrator=True)
 async def ban(ctx, member: discord.Member = None):
     if member is None:
         return await ctx.send("[ОШИБКА!] Укажите пользователя")
     await member.ban()
-    message: Discord.Message = await ctx.send(f"Пользователь {member} успешно забанен!")
+    await log_action('Бан', ctx.author, member)
+    message = await ctx.send(f"Пользователь {member} успешно забанен!")
     await message.add_reaction("✔️")
 
-
-# Исключение пользователя
-@commands.has_permissions( administrator = True )
-@client.command(pass_context=True, name="kick")
+# kick
+@bot.command()
+@commands.has_permissions(administrator=True)
 async def kick(ctx, member: discord.Member = None):
     if member is None:
         return await ctx.send("[ОШИБКА!] Укажите пользователя")
     await member.kick()
-    message: Discord.Message = await ctx.send(f"Пользователь {member} успешно кикнут!")
+    await log_action('Кик', ctx.author, member)
+    message = await ctx.send(f"Пользователь {member} успешно кикнут!")
     await message.add_reaction("✔️")
 
-
-# Мут пользователя
-@client.command(pass_context=True, name="mute")
-@commands.has_permissions( administrator = True )
-async def mute(ctx, member: discord.Member = None):
-    if member is None:
-        return await ctx.send("[ОШИБКА!] Проверьте правильность написания команды!")
-    role = discord.utils.get(ctx.guild.roles, id=1113127519634342064)
-    await member.add_roles(role)
-    message: Discord.Message = await ctx.send(f"Пользователь {member} успешно замучен!")
-    await message.add_reaction("✔️")
-
-
-# Очистка чата
-@client.command()
-@commands.has_permissions( administrator = True )
-async def clear(ctx, amount=None):
-    await ctx.channel.purge(limit=int(amount))
-    message: Discord.Message = await ctx.send(f"Сообщения успешно удалены!")
-    await message.add_reaction("✔️")
-
-
-# Сказать от имени бота
-@client.command(pass_context=True, name="say")
-@commands.has_permissions( administrator = True )
+# say
+@bot.command()
+@commands.has_permissions(administrator=True)
 async def say(ctx, *, arg):
     await ctx.send(arg)
     await ctx.message.delete()
+    await log_action('Команда say', ctx.author, ctx.channel)
 
+# запретки
+@bot.event
+async def on_message(message):
+    # Проверка, отправлено ли сообщение в канал и не является ли оно сообщением бота
+    if message.guild is not None and message.author != bot.user:
+        # Проверка запреток
+        for word in forbidden_words:
+            if word in message.content.lower():
+                # Удаление сообщения
+                await message.delete()
+                # Оповещение об удалении сообщения
+                await message.channel.send(f"{message.author.mention}, в вашем сообщении содержится запрещенное слово, настоятельно рекомендуем ознакомиться с правилами сервера и впредь не нарушать!")
+                break  # Выход из цикла после удаления сообщения
+    await bot.process_commands(message)  # Обработка команд
 
 # RUN
-client.run(config.TOKEN)
+bot.run(config.TOKEN)
